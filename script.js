1f1ab24e281b608ef2b9df81efe0f2a15ac15b58
Copaal10 <cortespalaciosalberto@gmail.com>
2025-11-24 12:00:45 -0500

    Actualización de tarjetas con let y mejoras en productos.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-
 // 1. Seleccionamos todas las tarjetas que tienen data-id
-var tarjetas = document.querySelectorAll(".card");
+let tarjetas = document.querySelectorAll(".card");
 
 // 2. Recorremos cada tarjeta
-for (var i = 0; i < tarjetas.length; i++) {
-  var card = tarjetas[i];                 // la casilla actual
-  var id = card.getAttribute("data-id");  // el número de producto
-  var producto = productos[id - 1];       // buscamos el producto en el array (posición id-1)
+for (let i = 0; i < tarjetas.length; i++) {
+  let card = tarjetas[i];                 // la casilla actual
+  let id = card.getAttribute("data-id");  // el número de producto
+  let producto = productos[id - 1];       // buscamos el producto en el array (posición id-1)
 
   // 3. Reemplazamos el contenido de la casilla
-  card.innerHTML = 
+  card.innerHTML =
     '<img src="' + producto.imagen + '" alt="' + producto.nombre + '" width="150">' +
     '<h5>' + producto.nombre + '</h5>' +
     '<p>Precio: $' + producto.precio + '</p>';
