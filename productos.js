065abaa71b1b5eba89b9abb39c99a0401fa552cc
Copaal10 <cortespalaciosalberto@gmail.com>
2025-11-24 11:47:26 -0500

    contenido de productoss

---
+++ productos.js
@@ -0,0 +1,123 @@
+// productos.js
+const productos = [
+  {
+    id: 1,
+    nombre: "Bañador clásico",
+    precio: 25.01,
+    imagen: "img/banador1.jpg"
+  },
+  {
+    id: 2,
+    nombre: "Bikini moderno",
+    precio: 29.99,
+    imagen: "img/bikini1.jpg"
+  },
+  {
+    id: 3,
+    nombre: "Short de natación",
+    precio: 19.99,
+    imagen: "img/short1.jpg"
+  },
+  {
+    id: 4,
+    nombre: "Gafas de natación",
+    precio: 15.50,
+    imagen: "img/gafas1.jpg"
+  },
+  {
+    id: 5,
+    nombre: "Gorro de silicona",
+    precio: 8.99,
+    imagen: "img/gorro1.jpg"
+  },
+  {
+    id: 6,
+    nombre: "Sandalias acuáticas",
+    precio: 22.00,
+    imagen: "img/sandalias1.jpg"
+  },
+  {
+    id: 7,
+    nombre: "Toalla microfibra",
+    precio: 12.50,
+    imagen: "img/toalla1.jpg"
+  },
+  {
+    id: 8,
+    nombre: "Bañador deportivo",
+    precio: 27.99,
+    imagen: "img/banador2.jpg"
+  },
+  {
+    id: 9,
+    nombre: "Bikini estampado",
+    precio: 31.99,
+    imagen: "img/bikini2.jpg"
+  },
+  {
+    id: 10,
+    nombre: "Traje completo mujer",
+    precio: 34.99,
+    imagen: "img/traje1.jpg"
+  },
+  {
+    id: 11,
+    nombre: "Traje completo hombre",
+    precio: 32.99,
+    imagen: "img/traje2.jpg"
+  },
+  {
+    id: 12,
+    nombre: "Chaleco flotador",
+    precio: 39.99,
+    imagen: "img/chaleco1.jpg"
+  },
+  {
+    id: 13,
+    nombre: "Aletas cortas",
+    precio: 45.00,
+    imagen: "img/aletas1.jpg"
+  },
+  {
+    id: 14,
+    nombre: "Bolso impermeable",
+    precio: 28.50,
+    imagen: "img/bolso1.jpg"
+  },
+  {
+    id: 15,
+    nombre: "Snorkel básico",
+    precio: 18.99,
+    imagen: "img/snorkel1.jpg"
+  },
+  {
+    id: 16,
+    nombre: "Snorkel profesional",
+    precio: 42.99,
+    imagen: "img/snorkel2.jpg"
+  },
+  {
+    id: 17,
+    nombre: "Kit entrenamiento (pull buoy + tabla)",
+    precio: 36.00,
+    imagen: "img/kit1.jpg"
+  },
+  {
+    id: 18,
+    nombre: "Protector solar resistente al agua",
+    precio: 14.99,
+    imagen: "img/protector1.jpg"
+  },
+  {
+    id: 19,
+    nombre: "Botella deportiva",
+    precio: 9.99,
+    imagen: "img/botella1.jpg"
+  },
+  {
+    id: 20,
+    nombre: "Camiseta UV",
+    precio: 24.99,
+    imagen: "img/camiseta1.jpg"
+  }
+];
