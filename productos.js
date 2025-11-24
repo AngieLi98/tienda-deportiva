3201d7367bb8f1fd6031f345f5740fde88d9c9cf
Lorena <pontongeidy@gmail.com>
2025-11-24 14:11:31 -0500

    agrugue imagenes

--- productos.js
+++ productos.js
@@ -2,122 +2,122 @@
 const productos = [
   {
     id: 1,
-    nombre: "Bañador clásico",
-    precio: 25.01,
-    imagen: "img/banador1.jpg"
+    nombre: "Vestido de baño clasico",
+    precio: 48900,
+    imagen: "img/Vestido de baño clasico.jpg.webp"
   },
   {
     id: 2,
-    nombre: "Bikini moderno",
-    precio: 29.99,
-    imagen: "img/bikini1.jpg"
+    nombre: "Vestido de baño manga larga",
+    precio: 106900,
+    imagen: "img/vestido-de-bano-manga-larga.jpg"
   },
   {
     id: 3,
     nombre: "Short de natación",
-    precio: 19.99,
-    imagen: "img/short1.jpg"
+    precio: 48000,
+    imagen: "img/short-surf-reva-mujer-negro.avif"
   },
   {
     id: 4,
-    nombre: "Gafas de natación",
-    precio: 15.50,
-    imagen: "img/gafas1.jpg"
+    nombre: "Vestido de Baño dos piezas",
+    precio: 98000,
+    imagen: "img/vestido de baño de dos pieza.webp"
   },
   {
     id: 5,
-    nombre: "Gorro de silicona",
-    precio: 8.99,
-    imagen: "img/gorro1.jpg"
+    nombre: "vestido de baño enterizo",
+    precio: 100600,
+    imagen: "img/vestido de baño enterizo.webp"
   },
   {
     id: 6,
-    nombre: "Sandalias acuáticas",
-    precio: 22.00,
-    imagen: "img/sandalias1.jpg"
+    nombre: "Top de vestido de Baño",
+    precio: 86900,
+    imagen: "img/Top de vestido de Baño.webp"
   },
   {
     id: 7,
-    nombre: "Toalla microfibra",
-    precio: 12.50,
-    imagen: "img/toalla1.jpg"
+    nombre: "Traje de Baño morado",
+    precio: 120.900,
+    imagen: "img/Traje de baño morado.webp"
   },
   {
     id: 8,
-    nombre: "Bañador deportivo",
-    precio: 27.99,
-    imagen: "img/banador2.jpg"
+    nombre: "vestido de baño bikini blanco",
+    precio: 147900,
+    imagen: "img/vestido de baño bikini blanco.webp"
   },
   {
     id: 9,
-    nombre: "Bikini estampado",
-    precio: 31.99,
-    imagen: "img/bikini2.jpg"
+    nombre: "Vestido de baño conjunto bikini",
+    precio: 89900,
+    imagen: "img/Vestido de baño conjunto bikini.webp"
   },
   {
     id: 10,
-    nombre: "Traje completo mujer",
-    precio: 34.99,
-    imagen: "img/traje1.jpg"
+    nombre: "Vestido de baño conjunto estampado",
+    precio: 126000,
+    imagen: "img/Vestido de baño conjunto estampada.webp"
   },
   {
     id: 11,
-    nombre: "Traje completo hombre",
-    precio: 32.99,
-    imagen: "img/traje2.jpg"
+    nombre: "Vestido de baño cargadera ancha y escote redondo",
+    precio: 154900,
+    imagen: "img/vestido de baño de cargaderas anchas y escote redondo.webp"
   },
   {
     id: 12,
-    nombre: "Chaleco flotador",
-    precio: 39.99,
-    imagen: "img/chaleco1.jpg"
+    nombre: "Vestido de baño eneterizo florales",
+    precio: 99900,
+    imagen: "img/Vestido de baño enterizo florales.webp"
   },
   {
     id: 13,
-    nombre: "Aletas cortas",
-    precio: 45.00,
-    imagen: "img/aletas1.jpg"
+    nombre: "Vestido de baño rojo con escote",
+    precio: 65900,
+    imagen: "img/vestido de baño rojo con escote.webp"
   },
   {
     id: 14,
-    nombre: "Bolso impermeable",
-    precio: 28.50,
-    imagen: "img/bolso1.jpg"
+    nombre: "Vestido de baño conjunto rojo",
+    precio: 134900,
+    imagen: "img/vestido de baño rojo.webp"
   },
   {
     id: 15,
-    nombre: "Snorkel básico",
-    precio: 18.99,
-    imagen: "img/snorkel1.jpg"
+    nombre: "Vestido de baño trikini",
+    precio: 117900,
+    imagen: "img/vestido de baño trikini.webp"
   },
   {
     id: 16,
-    nombre: "Snorkel profesional",
-    precio: 42.99,
-    imagen: "img/snorkel2.jpg"
+    nombre: "Vestido de baño manga larga",
+    precio: 167900,
+    imagen: "img/222272-800-1200.webp"
   },
   {
     id: 17,
-    nombre: "Kit entrenamiento (pull buoy + tabla)",
-    precio: 36.00,
-    imagen: "img/kit1.jpg"
+    nombre: "vestido de baño con tiras - blue",
+    precio: 89900,
+    imagen: "img/vestido-entero-natacion-natare-tiras-natacion-impulse-blue-2.webp"
   },
   {
     id: 18,
-    nombre: "Protector solar resistente al agua",
-    precio: 14.99,
-    imagen: "img/protector1.jpg"
+    nombre: "Vestido de baño manga larga negro con palmeras",
+    precio: 110900,
+    imagen: "img/vestido-bano-manga-larga-cierre-natacion-natare-negro-palmeras-3.webp"
   },
   {
     id: 19,
-    nombre: "Botella deportiva",
-    precio: 9.99,
-    imagen: "img/botella1.jpg"
+    nombre: "Vestido de Baño enterizo celeste",
+    precio: 95600,
+    imagen: "img/vestido-bano-entero-natacion-3back-natare-celeste-2.webp"
   },
   {
     id: 20,
-    nombre: "Camiseta UV",
-    precio: 24.99,
-    imagen: "img/camiseta1.jpg"
+    nombre: "Bikini Moderno",
+    precio: 54900,
+    imagen: "img/Bikini moderno.webp"
   }
 ];
